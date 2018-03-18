c06b1d535e4d979afed7b68f7d649f03eb15705b
Ankit-22 <ankitmani.t@somaiya.edu>
2018-03-18 22:42:01 +0530

    Rearrange

--- copy_all.sh
+++ copy_all.sh
@@ -1,3 +1,13 @@
-cp -f ~/.i3/config .
+if [ ! -d ./i3 ]; then
+	mkdir i3
+fi
+
+cp -f ~/.i3/config ./i3/
+
 cp -f ~/.zshrc .
-cp -f ~/.config/sublime-text-3/Packages/User/*.sublime-settings .
+
+if [ ! -d ./sublime-settings ]; then
+	mkdir sublime-settings
+fi
+
+cp -f ~/.config/sublime-text-3/Packages/User/*.sublime-settings ./sublime-settings/
